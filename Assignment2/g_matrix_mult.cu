aa32bbb8cf1abf6b53ba88499df2dc236ed73d5f
doxterpepper <dockconnellyoneal@gmail.com>
2017-02-14 23:32:40 -0500

    It works! Matrix multiplication using GPGPU works

--- Assignment2/g_matrix_mult.cu
+++ Assignment2/g_matrix_mult.cu
@@ -6,8 +6,8 @@ __global__ void g_mat_mult(double* m1, double* m2, double* m3, unsigned int size
   int c_j = threadIdx.y;
   double dot = 0;
   for (int i = 0; i < size; i++)
-    dot += m1[c_i * size + c_j];
-  m3[c_i*size + c_j];
+    dot += m1[c_i * size + i] * m2[i * size + c_j];
+  m3[c_i*size + c_j] = dot;
 }
 
 double* global_matrix_mult(const double* m1, const double* m2, unsigned int size)
@@ -18,7 +18,7 @@ double* global_matrix_mult(const double* m1, const double* m2, unsigned int size
   double* result;
   unsigned int num_bytes;
 
-  num_bytes = size * size * sizeof(float);
+  num_bytes = size * size * sizeof(double);
   result = (double*)malloc(num_bytes);
   cudaMalloc((void**) &c_m1, num_bytes);
   cudaMalloc((void**) &c_m2, num_bytes);
@@ -26,8 +26,9 @@ double* global_matrix_mult(const double* m1, const double* m2, unsigned int size
 
   cudaMemcpy(c_m1, m1, num_bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(c_m2, m2, num_bytes, cudaMemcpyHostToDevice);
+  dim3 threadsPerBlock(size, size);
 
-  g_mat_mult<<<size, size>>>(c_m1, c_m2, c_m3, size);
+  g_mat_mult<<<1, threadsPerBlock>>>(c_m1, c_m2, c_m3, size);
   cudaMemcpy(result, c_m3, num_bytes, cudaMemcpyDeviceToHost);
 
   cudaFree(c_m1);
