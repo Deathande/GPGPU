ca377d20409f5a8d75c114103215bd841f2d219b
doxterpepper <dockconnellyoneal@gmail.com>
2017-05-01 05:31:35 -0400

    Done here

--- Assignment6/main.cu
+++ Assignment6/main.cu
@@ -2,15 +2,17 @@
 #include <curand_kernel.h>
 #include <stdio.h>
 
+#define BLOCK_SIZE 1024
+
 __global__
 void mc_alg(float a, float b, float height, float (*f)(float), unsigned int* num)
 {
-  int c_i = threadIdx.x;
   curandState* state;
-  float r = curand_uniform(state);
-  *num = 5;
-  printf("%d\n", *num);
+  float x = (float)curand(state) / (float)RAND_MAX / (b-a) + a;
+  float y = (float)curand(state) / (float)RAND_MAX / (height-0) + 0;
   printf("here");
+  if (f(x) >= y)
+    atomicAdd(num, 1);
 }
 
 float function(float x)
@@ -20,16 +22,27 @@ float function(float x)
 
 int main()
 {
-  float h_a;
-  float h_b;
-  float h_h;
+  float h_a = 0;
+  float h_b = 10;
+  float h_h = 10;
+  //float d_a;
+  //float d_b;
+  //float d_h;
+  float ans;
   unsigned int* num;
   unsigned int h_n = 0;
 
   cudaMalloc((void**) &num, sizeof(unsigned int));
   cudaMemcpy(&h_n, num, sizeof(unsigned int), cudaMemcpyHostToDevice);
 
-  mc_alg<<<1, 1024>>>(h_a, h_b, h_h, &function, num);
+  dim3 dimBlock(BLOCK_SIZE, 1, 1);
+  dim3 dimGrid(ceil((float)10 / (float)dimBlock.x), 1, 1);
+
+  mc_alg<<<dimGrid,dimBlock>>>(h_a, h_b, h_h, &function, num);
+
+  // can't get this to return anything...
   cudaMemcpy(num, &h_n, sizeof(unsigned int), cudaMemcpyDeviceToHost);
-  printf("%d\n", h_n);
+  ans = h_n / (dimGrid.x * BLOCK_SIZE);
+  printf("%f\n", ans);
+  cudaFree(num);
 }
