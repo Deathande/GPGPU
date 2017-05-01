7fa64ed8cb4e361115b8fc617e96cf664164c03e
dock.oneal <doxterpeper@gmail.com>
2017-05-01 05:05:39 -0400

    upading the repo so I can switch to my desktop

--- Assignment6/main.cu
+++ Assignment6/main.cu
@@ -1,14 +1,35 @@
 #include <cuda.h>
-#include <cudarand.h>
+#include <curand_kernel.h>
 #include <stdio.h>
 
 __global__
 void mc_alg(float a, float b, float height, float (*f)(float), unsigned int* num)
 {
   int c_i = threadIdx.x;
-  cudarandState state;
+  curandState* state;
+  float r = curand_uniform(state);
+  *num = 5;
+  printf("%d\n", *num);
+  printf("here");
+}
+
+float function(float x)
+{
+  return x * x;
 }
 
 int main()
 {
+  float h_a;
+  float h_b;
+  float h_h;
+  unsigned int* num;
+  unsigned int h_n = 0;
+
+  cudaMalloc((void**) &num, sizeof(unsigned int));
+  cudaMemcpy(&h_n, num, sizeof(unsigned int), cudaMemcpyHostToDevice);
+
+  mc_alg<<<1, 1024>>>(h_a, h_b, h_h, &function, num);
+  cudaMemcpy(num, &h_n, sizeof(unsigned int), cudaMemcpyDeviceToHost);
+  printf("%d\n", h_n);
 }
