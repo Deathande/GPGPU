3acf0029dc9f41e7e6af30f6e55f5d5cd7eef58f
doxterpepper <dockconnellyoneal@gmail.com>
2017-05-02 02:37:42 -0400

    Finished the cuda code
    - I was finding the area of the wrong part of the box
    - my box height was too high
    - Would be better with a function to find the max of the function

--- Assignment6/main.cu
+++ Assignment6/main.cu
@@ -6,13 +6,18 @@
 
 #define BLOCK_SIZE 1024
 
+__device__ float function(float x)
+{
+  return x * x;
+}
+
 __global__ void init(unsigned int seed, curandState_t* states)
 {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   curand_init(seed, blockIdx.x, 0, &states[id]);
 }
 
-__global__ void test(int* ret,
+__global__ void mc(int* ret,
                      curandState_t* states,
                      float a,
                      float b,
@@ -22,19 +27,8 @@ __global__ void test(int* ret,
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   float x = curand_uniform(&states[i]) * (b-a) + a;
   float y = curand_uniform(&states[i]) * (h-0) + 0;
-  //printf("ID: %d f(x): %f y: %f\n", i, f(x), y);
-
-  printf("ID: %d x: %f y: %f\n", i, x, y);
-  if (x * x < y)
-  {
+  if (function(x) >= y)
     atomicAdd(ret, 1);
-    printf("%d\n", *ret);
-  }
-}
-
-__device__ float function(float x)
-{
-  return x * x;
 }
 
 int main()
@@ -43,21 +37,30 @@ int main()
   int* d_a;
   float a = 1.0;
   float b = 10.0;
-  float h = 13.0;
+  float h = 150.0;
+  unsigned int grid = 1000;
   
   curandState_t* states;
-  unsigned int n = BLOCK_SIZE;
+  unsigned int n = BLOCK_SIZE * grid;
 
   h_a = (int*)malloc(sizeof(int));
   *h_a = 0;
+
   cudaMalloc((void**)&d_a, sizeof(int));
   cudaMalloc((void**)&states, n * sizeof(curandState_t));
+
   cudaMemcpy(d_a, h_a, sizeof(int), cudaMemcpyHostToDevice);
-  init<<<BLOCK_SIZE, 1>>>((unsigned int) time(NULL), states);
-  test<<<BLOCK_SIZE, 1>>>(d_a, states, a, b, h, &function);
+
+  dim3 dimBlock(BLOCK_SIZE, 1, 1);
+  dim3 dimGrid(grid, 1, 1);
+
+  init<<<dimGrid, dimBlock>>>((unsigned int) time(NULL), states);
+  mc<<<dimGrid, dimBlock>>>(d_a, states, a, b, h, &function);
+
   cudaMemcpy(h_a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
   printf("return: %d\n", *h_a);
-  float result = (b - a) * h * ((float)*h_a / (float)n);
+  float result = ((b - a) * h) * ((float)*h_a / (float)n);
   printf("answer: %f\n", result);
   cudaFree(d_a);
 }
